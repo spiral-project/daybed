b1610766153217ec42032a13f42844b1209d86fc
Mathieu Leplatre <contact@mathieu-leplatre.info>
2012-05-31 01:16:14 +0200

    Simple and stupid type registry (ref #2)

--- daybed/schemas.py
+++ daybed/schemas.py
@@ -11,9 +11,72 @@ from colander import (
 )
 
 
-class UnknownFieldTypeError(Exception):
+class AlreadyRegisteredError(Exception):
+    pass
+
+class NotRegisteredError(Exception):
+    pass
+
+class UnknownFieldTypeError(NotRegisteredError):
     """ Raised if schema contains a field with unknown type. """
     pass
+
+
+class TypeRegistry(object):
+    def __init__(self):
+        self._registry = {}
+
+    def register(self, name, klass):
+            if name in self._registry:
+                raise AlreadyRegisteredError('The type %s is already registered' % name)
+            self._registry[name] = klass
+
+    def unregister(self, name):
+        if model not in self._registry:
+            raise NotRegisteredError('The model %s is not registered' % name)
+        del self._registry[name]
+
+    def schema(self, typename, **options):
+        try:
+            nodetype = self._registry[typename]
+        except KeyError:
+            raise UnknownFieldTypeError('Type "%s" is unknown' % name)
+        return nodetype(**options)
+
+    @property
+    def names(self):
+        return self._registry.keys()
+
+
+types = TypeRegistry()
+
+
+class TypeField(SchemaNode):
+    def __init__(self, node, **kwargs):
+        options = dict(name='', 
+                       description='',
+                       validator=None)
+        options.update(**kwargs)
+        super(TypeField, self).__init__(node, **options)
+
+
+class IntField(TypeField):
+    def __init__(self, **kwargs):
+        super(IntField, self).__init__(Int(), **kwargs)
+types.register('int', IntField)
+
+
+class StringField(TypeField):
+    def __init__(self, **kwargs):
+        super(StringField, self).__init__(String(), **kwargs)
+types.register('string', StringField)
+
+
+class EnumField(TypeField):
+    def __init__(self, **kwargs):
+        kwargs['validator'] = OneOf(kwargs['choices'])
+        super(EnumField, self).__init__(String(), **kwargs)
+types.register('enum', EnumField)
 
 
 class ModelChoices(TupleSchema):
@@ -22,7 +85,7 @@ class ModelChoices(TupleSchema):
 
 class ModelField(MappingSchema):
     name = SchemaNode(String())
-    type = SchemaNode(String(), validator=OneOf(('int', 'string', 'enum')))
+    type = SchemaNode(String(), validator=OneOf(types.names))
     description = SchemaNode(String())
     # TODO: TBD in subclassing, a way to have optional fields. Cross Field ?
     # choices = ModelChoices()
@@ -43,17 +106,5 @@ class SchemaValidator(SchemaNode):
         super(SchemaValidator, self).__init__(Mapping())
         for field in definition['fields']:
             fieldtype = field['type']
-            validator = None
-            # TODO: implement a type registry (issue #2)
-            if fieldtype == 'int':
-                nodetype = Int()
-            elif fieldtype == 'string':
-                nodetype = String()
-            elif fieldtype == 'enum':
-                nodetype = String()
-                validator = OneOf(field['choices'])
-            else:
-                raise UnknownFieldTypeError('Type "%s" is unknown' % fieldtype)
-            self.add(SchemaNode(nodetype, name=field['name'],
-                                description=field['description'],
-                                validator=validator))
+            self.add(types.schema(fieldtype, **field))
+
