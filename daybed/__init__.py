c48440c3b8c93e0b6209ce58c6a41c0867d2d7bb
Rémy HUBSCHER <hubscher.remy@gmail.com>
2012-10-20 01:39:02 +0200

    If database doesn't exists, create and log the action.

--- daybed/__init__.py
+++ daybed/__init__.py
@@ -1,11 +1,14 @@
 """Main entry point
 """
-import couchdb
+import logging
+from couchdb.client import Server
+from couchdb.http import PreconditionFailed
 
 from pyramid.config import Configurator
 from pyramid.events import NewRequest
 from daybed.db import DatabaseConnection, sync_couchdb_views
 
+logger = logging.getLogger('daybed')
 
 def add_db_to_request(event):
     request = event.request
@@ -13,14 +16,14 @@ def add_db_to_request(event):
     con_info = settings['db_server'][settings['db_name']]
     event.request.db = DatabaseConnection(con_info)
 
-def create_db_if_not_exist(server, db_name, exist = False):
-    try:
-        if not exist:
-            sync_couchdb_views(server[db_name])
-    except Exception, e:
-        db = server.create(db_name)
-        create_db_if_not_exist(server, db_name, True)
 
+def create_db_if_not_exist(server, db_name):
+    try:
+        server.create(db_name)
+        logger.info('CouchDB database "%s" successfully created.' % db_name)
+    except PreconditionFailed:
+        logger.info('CouchDB database "%s" already exists.' % db_name)
+        pass
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
@@ -28,8 +31,9 @@ def main(global_config, **settings):
     config.scan("daybed.views")
 
     # CouchDB initialization
-    db_server = couchdb.client.Server(settings['couchdb_uri'])
+    db_server = Server(settings['couchdb_uri'])
     config.registry.settings['db_server'] = db_server
-    create_db_if_not_exist(db_server, settings['db_name'], False)
+    create_db_if_not_exist(db_server, settings['db_name'])
+    sync_couchdb_views(db_server[settings['db_name']])
     config.add_subscriber(add_db_to_request, NewRequest)
     return config.make_wsgi_app()
