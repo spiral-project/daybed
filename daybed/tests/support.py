64398ae731b2f89e126ae9c63fe048134cbf649c
Alexis Métaireau <alexis@notmyidea.org>
2012-10-21 15:11:08 +0200

    Create a random db for the tests each time.

--- daybed/tests/support.py
+++ daybed/tests/support.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 from unittest import TestCase
 import webtest
 
@@ -13,12 +14,14 @@ class BaseWebTest(TestCase):
     """
 
     def setUp(self):
+        self.db_name = os.environ['DB_NAME'] = 'daybed-tests-%s' % uuid4()
+
         self.app = webtest.TestApp("config:tests.ini", relative_to=HERE)
         self.db_server = self.app.app.registry.settings['db_server']
 
     def tearDown(self):
         # Delete Test DB
-        del self.db_server[self.app.app.registry.settings['db_name']]
+        del self.db_server[self.db_name]
 
     def put_valid_definition(self):
         """Create a valid definition named "todo".
