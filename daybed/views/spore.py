4b5e0dc6d1afb56672b6da55f5275f10c386eda6
Alexis Métaireau <alexis@notmyidea.org>
2012-10-22 13:11:13 +0200

    Add a test for spore, update to the latest cornice version

--- daybed/views/spore.py
+++ daybed/views/spore.py
@@ -1,5 +1,5 @@
 from cornice import Service
-from cornice.spore import generate_spore
+from cornice.ext.spore import generate_spore_description
 from cornice.service import get_services
 
 from daybed import VERSION
@@ -9,6 +9,8 @@ spore = Service(name="spore",
                 description="Spore endpoint",
                 renderer="jsonp")
 
+
 @spore.get()
 def get_spore(request):
-    return generate_spore(get_services(), 'daybed', request.application_url, VERSION)
+    return generate_spore_description(get_services(), 'daybed',
+              request.application_url, VERSION)
