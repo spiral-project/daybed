cec8a88b551c2903443c56c00d6744b94574af9a
Mathieu Leplatre <contact@mathieu-leplatre.info>
2012-12-08 10:50:19 +0100

    Prepare inheritance of functional tests (ref #31)

--- daybed/tests/test_functional.py
+++ daybed/tests/test_functional.py
@@ -14,6 +14,7 @@ class FunctionalTest(BaseWebTest):
 
     def __init__(self, *args, **kwargs):
         super(FunctionalTest, self).__init__(*args, **kwargs)
+        self.model_name = 'todo'
         self.valid_definition = {
             "title": "todo",
             "description": "A list of my stuff to do",
@@ -44,21 +45,21 @@ class FunctionalTest(BaseWebTest):
     def create_definition(self, data=None):
         if not data:
             data = self.valid_definition
-
-        return self.app.put_json('/definitions/todo', data,
+        return self.app.put_json('/definitions/%s' % self.model_name,
+                                 data,
                                  headers=self.headers)
 
     def create_data(self, data=None):
         if not data:
             data = self.valid_data
-        return self.app.post_json('/data/todo', data,
+        return self.app.post_json('/data/%s' % self.model_name,
+                                  data,
                                   headers=self.headers)
 
     def create_data_resp(self, data=None):
         if not data:
             data = self.valid_data
-
-        return self.app.post_json('/data/todo',
+        return self.app.post_json('/data/%s' % self.model_name,
                                   data,
                                   headers=self.headers)
 
@@ -67,14 +68,14 @@ class FunctionalTest(BaseWebTest):
         self.assertIn('token', resp.body)
 
     def test_malformed_definition_creation(self):
-        resp = self.app.put_json('/definitions/todo',
+        resp = self.app.put_json('/definitions/%s' % self.model_name,
                     self.definition_without_title,
                     headers=self.headers,
                     status=400)
         self.assertIn('"name": "title"', resp.body)
 
     def test_definition_creation_rejects_malformed_data(self):
-        resp = self.app.put('/definitions/todo',
+        resp = self.app.put('/definitions/%s' % self.model_name,
                     self.malformed_definition,
                     headers=self.headers,
                     status=400)
@@ -84,7 +85,8 @@ class FunctionalTest(BaseWebTest):
         self.create_definition()
 
         # Verify that the schema is the same
-        resp = self.app.get('/definitions/todo', headers=self.headers)
+        resp = self.app.get('/definitions/%s' % self.model_name,
+                            headers=self.headers)
         self.assertEqual(resp.json, self.valid_definition)
 
     def test_definition_deletion(self):
@@ -92,17 +94,19 @@ class FunctionalTest(BaseWebTest):
         token = resp.json['token']
         resp = self.create_data()
         data_item_id = resp.json['id']
-        self.app.delete(str('/definitions/todo?token=%s' % token))
-        queryset = self.db.get_data_item('todo', data_item_id)
+        self.app.delete(str('/definitions/%s?token=%s' % (
+                            self.model_name, token)))
+        queryset = self.db.get_data_item(self.model_name,
+                                         data_item_id)
         self.assertIsNone(queryset)
-        queryset = self.db.get_definition('todo')
+        queryset = self.db.get_definition(self.model_name)
         self.assertIsNone(queryset)
 
     def test_normal_data_creation(self):
         self.create_definition()
 
         # Put data against this definition
-        resp = self.app.post_json('/data/todo',
+        resp = self.app.post_json('/data/%s' % self.model_name,
                                  self.valid_data,
                                  headers=self.headers)
         self.assertIn('id', resp.body)
@@ -111,11 +115,11 @@ class FunctionalTest(BaseWebTest):
         self.create_definition()
 
         # Try to put invalid data to this definition
-        resp = self.app.post_json('/data/todo',
-                                 {'item': 'My task',
-                                  'status': 'false'},
-                                 headers=self.headers,
-                                 status=400)
+        resp = self.app.post_json('/data/%s' % self.model_name,
+                                  {'item': 'My task',
+                                   'status': 'false'},
+                                  headers=self.headers,
+                                  status=400)
         self.assertIn('"status": "error"', resp.body)
 
     def test_data_retrieval(self):
@@ -126,7 +130,8 @@ class FunctionalTest(BaseWebTest):
         self.assertIn('id', resp.body)
 
         data_item_id = resp.json['id']
-        resp = self.app.get('/data/todo/%s' % data_item_id,
+        resp = self.app.get('/data/%s/%s' % (self.model_name,
+                                             data_item_id),
                             headers=self.headers)
         entry = self.valid_data.copy()
         # entry['id'] = str(data_item_id
@@ -141,31 +146,38 @@ class FunctionalTest(BaseWebTest):
 
         # Update this data
         entry['status'] = 'done'
-        resp = self.app.put_json(str('/data/todo/%s' % data_item_id),
+        resp = self.app.put_json(str('/data/%s/%s' % (
+                                     self.model_name,
+                                     data_item_id)),
                                  entry,
                                  headers=self.headers)
         self.assertIn('id', resp.body)
         # Todo : Verify DB
-        queryset = self.db.get_data('todo')
+        queryset = self.db.get_data(self.model_name)
         self.assertEqual(len(queryset), 1)
 
     def test_data_deletion(self):
         self.create_definition()
         resp = self.create_data()
         data_item_id = resp.json['id']
-        self.app.delete(str('/data/todo/%s' % data_item_id))
-        queryset = self.db.get_data_item('todo', data_item_id)
+        self.app.delete(str('/data/%s/%s' % (self.model_name,
+                                             data_item_id)))
+        queryset = self.db.get_data_item(self.model_name,
+                                         data_item_id)
         self.assertIsNone(queryset)
 
     def test_data_validation(self):
         self.create_definition()
         headers = self.headers.copy()
         headers['X-Daybed-Validate-Only'] = 'true'
-        self.app.post_json('/data/todo', self.valid_data,
+        self.app.post_json('/data/%s' % self.model_name,
+                           self.valid_data,
                            headers=headers, status=200)
 
         # no data should be added
-        self.assertEquals(0, len(self.app.get('/data/todo').json['data']))
+        response = self.app.get('/data/%s' % self.model_name)
+        self.assertEquals(0, len(response.json['data']))
         # of course, pushing weird data should tell what's wrong
-        self.app.post_json('/data/todo', self.invalid_data,
+        self.app.post_json('/data/%s' % self.model_name,
+                           self.invalid_data,
                            headers=headers, status=400)
