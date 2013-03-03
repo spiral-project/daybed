c1a4f093aef05fcd1c654c42d80db102e6f8d8de
Antoine Cezar <antoine.cear@gmail.com>
2013-03-03 23:09:22 +0100

    Adapt tests to the new backend system

--- daybed/tests/support.py
+++ daybed/tests/support.py
@@ -1,15 +1,11 @@
-import os
-from uuid import uuid4
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest  # NOQA
 
 import webtest
-from daybed.db import DatabaseConnection
 
-
-HERE = os.path.dirname(os.path.abspath(__file__))
+from daybed.backends.couch_db.database import Database
 
 
 class BaseWebTest(unittest.TestCase):
@@ -20,15 +16,12 @@ class BaseWebTest(unittest.TestCase):
 
     def setUp(self):
 
-        self.db_name = os.environ['DB_NAME'] = 'daybed-tests-%s' % uuid4()
-        self.app = webtest.TestApp("config:tests.ini", relative_to=HERE)
-        self.db_server = self.app.app.registry.settings['db_server']
-        self.db_base = self.db_server[self.db_name]
-        self.db = DatabaseConnection(self.db_base)
+        self.app = webtest.TestApp("config:tests.ini", relative_to='.')
+        self.backend = self.app.app.registry.backend
+        self.db = Database(self.backend.db)
 
     def tearDown(self):
-        # Delete Test DB
-        self.db_server.delete(self.db_name)
+        self.backend.delete_db()
 
     def put_valid_definition(self):
         """Create a valid definition named "todo".
