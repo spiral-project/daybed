6ad319af60d806ffe7c656a0b26a8c6dc989ea08
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2014-01-04 18:32:24 +0100

    Upgrade to setuptools 2.0 to enable wheel and Python3 support

--- setup.py
+++ setup.py
@@ -1,9 +1,70 @@
 #!/usr/bin/env python
+import os
+import sys
+
 from setuptools import setup
 
-setup(setup_requires=['d2to1'],
-      d2to1=True,
+here = os.path.abspath(os.path.dirname(__file__))
+PY2 = sys.version_info[0] == 2
 
-      # This ensures that the MANIFEST.in is read, but it
-      # will become the default in distutils2.
-      include_package_data=True,)
+NAME = 'daybed'
+DESCRIPTION = 'Form validation and data storage API.'
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read().strip()
+AUTHOR = u'Spiral Project'
+EMAIL = u'spiral-project@lolnet.org'
+URL = 'https://{name}.readthedocs.org/'.format(name=NAME)
+CLASSIFIERS = ['Development Status :: 4 - Beta',
+               'License :: OSI Approved :: BSD License',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 2.6',
+               'Topic :: Internet :: WWW/HTTP',
+               'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+               'Framework :: Pyramid']
+KEYWORDS = ['web',
+            'cornice',
+            'couchdb',
+            'colander',
+            'pyramid',
+            'pylons',
+            'api',
+            'form',
+            'angular',
+            'backbone',
+            'storage']
+PACKAGES = [NAME.replace('-', '_')]
+REQUIREMENTS = ['setuptools',
+                'pyramid',
+                'cornice',
+                'colander',
+                'couchdb',
+                'pyramid_persona',
+                'pyramid_multiauth',
+                'pyramid_mako']
+DEPENDENCY_LINKS = [
+    'https://github.com/lilydjwg/couchdb-python3/tarball/master#egg=couchdb',
+]
+ENTRY_POINTS = {
+    'paste.app_factory': [
+        'main = daybed:main',
+    ]}
+
+
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(name=NAME,
+          version=VERSION,
+          description=DESCRIPTION,
+          long_description=README,
+          classifiers=CLASSIFIERS,
+          keywords=' '.join(KEYWORDS),
+          author=AUTHOR,
+          author_email=EMAIL,
+          url=URL,
+          license='BSD',
+          packages=PACKAGES,
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=REQUIREMENTS,
+          dependency_links=DEPENDENCY_LINKS,
+          entry_points=ENTRY_POINTS)
