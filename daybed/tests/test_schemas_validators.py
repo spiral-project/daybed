26eedfc381aed54b518526daa34c4af562b6522c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-10 10:36:20 +0100

    Start unit-testing validators

--- daybed/tests/test_schemas_validators.py
+++ daybed/tests/test_schemas_validators.py
@@ -1,7 +1,19 @@
+import mock
 import colander
+from cornice.errors import Errors
+from pyramid.testing import DummyRequest
 
-from daybed.schemas.validators import RolesValidator
+from daybed.schemas.validators import validator, RolesValidator
 from daybed.tests.support import unittest
+
+
+class ValidatorTests(unittest.TestCase):
+    def test_adds_body_error_if_json_invalid(self):
+        request = DummyRequest()
+        request.body = '{wrong,"format"}'
+        request.errors = Errors()
+        validator(request, mock.Mock())
+        self.assertEqual('body', request.errors[0]['location'])
 
 
 class RolesValidatorTests(unittest.TestCase):
