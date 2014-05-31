06280ea977b9d34b281cfd52ac326428d077053d
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-31 15:06:54 +0200

    Refactor object field tests (ref #54)

--- daybed/tests/test_schemas_object.py
+++ daybed/tests/test_schemas_object.py
@@ -4,35 +4,33 @@ from daybed import schemas
 from daybed.tests.support import BaseWebTest
 
 
-class ObjectFieldTest(BaseWebTest):
+OBJECT_FIELD_DEFINITION = {
+    'name': u'status',
+    'type': u'object',
+    'hint': u'An object',
+    'label': u'',
+    'required': True,
+    'fields': [
+        {'type': u'boolean',
+         'name': u'done',
+         'label': u'',
+         'hint': u'',
+         'required': True},
+        {'type': u'datetime',
+         'name': u'updated',
+         'label': u'',
+         'hint': u'',
+         'required': True,}
+    ]
+}
+
+
+class InvalidObjectFieldTest(BaseWebTest):
 
     def setUp(self):
-        super(ObjectFieldTest, self).setUp()
+        super(InvalidObjectFieldTest, self).setUp()
         self.schema = schemas.ObjectField.definition()
-        self.definition = {
-            'name': u'status',
-            'type': u'object',
-            'hint': u'An object',
-            'label': u'',
-            'required': True,
-            'fields': [{'type': u'boolean',
-                        'name': u'done'},
-                       {'type': u'datetime',
-                        'name': u'updated'}]}
-        self.validator = schemas.ListField.validation(**self.definition)
-
-    def _create_definition(self, **kwargs):
-        fakedef = {'title': 'stupid', 'description': 'stupid',
-                   'fields': [{"name": "age", "type": "int",
-                               "required": False}]}
-        fakedef.update(**kwargs)
-        return self.app.put_json('/models/simple',
-                                 {'definition': fakedef},
-                                 headers=self.headers)
-
-    def test_is_defined_without_model_and_fields(self):
-        self.definition.pop('fields')
-        self.schema.deserialize(self.definition)
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
 
     def test_is_not_valid_if_both_fields_and_model(self):
         self.definition['model'] = 'Foo'
@@ -40,29 +38,12 @@ class ObjectFieldTest(BaseWebTest):
                           self.schema.deserialize,
                           self.definition)
 
-    def test_is_defined_with_an_existing_model(self):
-        self.definition.pop('fields')
-        self._create_definition()
-        self.definition['model'] = 'simple'
-        self.schema.deserialize(self.definition)
-
     def test_is_not_valid_if_model_unknown(self):
         self.definition.pop('fields')
         self.definition['model'] = 'simple'
         self.assertRaises(colander.Invalid,
                           self.schema.deserialize,
                           self.definition)
-
-    def test_is_defined_with_valid_fields(self):
-        field = self.schema.deserialize(self.definition)
-        default_attrs = {
-            'label': u'',
-            'hint': u'',
-            'required': True,
-        }
-        self.definition['fields'][0].update(default_attrs)
-        self.definition['fields'][1].update(default_attrs)
-        self.assertDictEqual(self.definition, field)
 
     def test_is_not_valid_if_field_structure_invalid(self):
         self.definition['fields'][0].pop('type')
@@ -81,3 +62,50 @@ class ObjectFieldTest(BaseWebTest):
         self.assertRaises(colander.Invalid,
                           self.schema.deserialize,
                           self.definition)
+
+
+class FieldsObjectTest(BaseWebTest):
+    def setUp(self):
+        super(FieldsObjectTest, self).setUp()
+        self.schema = schemas.ObjectField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        # self.validator = schemas.ListField.validation(**self.definition)
+
+    def test_is_defined_with_valid_fields(self):
+        field = self.schema.deserialize(self.definition)
+        self.assertDictEqual(self.definition, field)
+
+
+class FreeObjectTest(BaseWebTest):
+    def setUp(self):
+        super(FreeObjectTest, self).setUp()
+        self.schema = schemas.ObjectField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        self.definition.pop('fields')
+
+    def test_is_defined_without_model_and_fields(self):
+        field = self.schema.deserialize(self.definition)
+        self.assertDictEqual(self.definition, field)
+
+
+class ModelFieldTest(BaseWebTest):
+    def setUp(self):
+        super(ModelFieldTest, self).setUp()
+        self.schema = schemas.ObjectField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        self.definition.pop('fields')
+        self._create_definition()
+        self.definition['model'] = 'simple'
+
+    def _create_definition(self, **kwargs):
+        fakedef = {'title': 'stupid', 'description': 'stupid',
+                   'fields': [{"name": "age", "type": "int",
+                               "required": False}]}
+        fakedef.update(**kwargs)
+        return self.app.put_json('/models/simple',
+                                 {'definition': fakedef},
+                                 headers=self.headers)
+
+    def test_is_defined_with_an_existing_model(self):
+        field = self.schema.deserialize(self.definition)
+        self.assertDictEqual(self.definition, field)
