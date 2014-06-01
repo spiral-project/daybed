0e319ff8cc78129f0e38b2fde2d4a64a837a0840
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-06-01 14:34:47 +0200

    Test list of objects (fixes #54)

--- daybed/schemas/list.py
+++ daybed/schemas/list.py
@@ -1,7 +1,30 @@
 from pyramid.i18n import TranslationString as _
-from colander import (SchemaNode, String, drop, OneOf)
+from colander import (SchemaNode, String, Sequence, drop, OneOf,
+                      Invalid, Mapping)
 
 from .base import registry, TypeField, JSONList
+
+
+class ParametersMatchItemType(object):
+    def __call__(self, node, value):
+        if 'parameters' in value and 'itemtype' not in value:
+            msg = u'Missing itemtype since parameters is provided'
+            raise Invalid(node, msg)
+        if 'itemtype' in value and 'parameters' in value:
+            itemtype = value['itemtype']
+            parameters = value['parameters']
+            schema = registry.definition(itemtype)
+            parameters.update(name='fake', type=itemtype)
+            schema.deserialize(parameters)
+
+
+class ItemsValid(object):
+    def __init__(self, fieldtype, **options):
+        nodetype = registry.validation(fieldtype, **options)
+        self.schema = SchemaNode(Sequence(), nodetype)
+
+    def __call__(self, node, value):
+        self.schema.deserialize(value)
 
 
 @registry.add('list')
@@ -16,4 +39,16 @@ class ListField(TypeField):
                               name='itemtype',
                               validator=OneOf(registry.names),
                               missing=drop))
+        schema.add(SchemaNode(Mapping(unknown='preserve'),
+                              name='parameters',
+                              missing=drop))
+        schema.validator = ParametersMatchItemType()
         return schema
+
+    @classmethod
+    def validation(cls, **kwargs):
+        if 'itemtype' in kwargs:
+            itemparameters = kwargs.get('parameters', {})
+            kwargs['validator'] = ItemsValid(kwargs['itemtype'],
+                                             **itemparameters)
+        return super(ListField, cls).validation(**kwargs)
--- daybed/tests/test_schemas_list.py
+++ daybed/tests/test_schemas_list.py
@@ -1,21 +1,24 @@
 import colander
 
 from daybed import schemas
-from daybed.tests.support import unittest
+from daybed.tests.support import unittest, BaseWebTest
+
+
+OBJECT_FIELD_DEFINITION = {
+    'name': u'tasks',
+    'type': u'list',
+    'hint': u'An object',
+    'label': u'',
+    'required': True,
+    'itemtype': u'int'
+}
 
 
 class ListFieldTest(unittest.TestCase):
 
     def setUp(self):
         self.schema = schemas.ListField.definition()
-        self.definition = {
-            'name': u'tasks',
-            'type': u'list',
-            'hint': u'An object',
-            'label': u'',
-            'required': True,
-            'itemtype': u'int'
-        }
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
         self.validator = schemas.ListField.validation(**self.definition)
 
     def test_can_be_defined_with_a_field_type(self):
@@ -28,6 +31,88 @@ class ListFieldTest(unittest.TestCase):
                           self.schema.deserialize,
                           self.definition)
 
-    def test_validation_succeeds_if_fields_are_valid(self):
+    def test_validation_succeeds_if_items_are_valid(self):
         value = self.validator.deserialize('[1,3,4]')
         self.assertEquals(value, [1, 3, 4])
+
+    def test_validation_succeeds_if_no_items(self):
+        value = self.validator.deserialize('[]')
+        self.assertEquals(value, [])
+
+    def test_validation_fails_if_items_malformed(self):
+        self.assertRaises(colander.Invalid,
+                          self.validator.deserialize,
+                          '[1,,3]')
+
+    def test_validation_fails_if_items_are_invalid(self):
+        self.assertRaises(colander.Invalid,
+                          self.validator.deserialize,
+                          '[1, "a" ,4]')
+
+
+class NoItemTypeListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.schema = schemas.ListField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        self.definition.pop('itemtype')
+        self.validator = schemas.ListField.validation(**self.definition)
+
+    def test_validation_succeeds_if_items_are_valid(self):
+        value = self.validator.deserialize('[1,"a",{"status": false}]')
+        self.assertEquals(value, [1, u'a', {u'status': False}])
+
+
+class ItemTypeListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.schema = schemas.ListField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        self.definition['itemtype'] = 'date'
+        self.validator = schemas.ListField.validation(**self.definition)
+
+    def test_validation_succeeds_if_items_are_valid(self):
+        value = self.validator.deserialize('["2012-04-01", "2014-06-01"]')
+        self.assertEquals(value, ["2012-04-01", "2014-06-01"])
+
+    def test_validation_succeeds_if_items_are_comma_separated(self):
+        value = self.validator.deserialize('2012-04-01, 2014-06-01')
+        self.assertEquals(value, ["2012-04-01", "2014-06-01"])
+
+    def test_validation_fails_if_items_are_invalid(self):
+        self.assertRaises(colander.Invalid,
+                          self.validator.deserialize,
+                          '["2012-33-01", "2014-06-01"]')
+
+
+class ObjectListTest(BaseWebTest):
+
+    def setUp(self):
+        super(ObjectListTest, self).setUp()
+        self.schema = schemas.ListField.definition()
+        self.definition = OBJECT_FIELD_DEFINITION.copy()
+        self.definition['itemtype'] = 'object'
+        self.definition['parameters'] = {
+            'fields': [
+                {'type': u'enum',
+                 'name': u'status',
+                 'choices': [u'todo', u'done']}
+            ]
+        }
+        self.validator = schemas.ListField.validation(**self.definition)
+
+    def test_is_not_valid_if_parameters_are_invalid(self):
+        self.definition['parameters']['fields'] = []
+        self.assertRaises(colander.Invalid,
+                          self.schema.deserialize,
+                          self.definition)
+
+    def test_validation_succeeds_if_items_are_valid(self):
+        value = self.validator.deserialize('[{"status": "todo"},'
+                                           ' {"status": "done"}]')
+        self.assertEquals(value, [{'status': 'todo'}, {'status': 'done'}])
+
+    def test_validation_fails_if_items_are_invalid(self):
+        self.assertRaises(colander.Invalid,
+                          self.validator.deserialize,
+                          '[{"status": "todo"}, {"status": "3.14"}]')
