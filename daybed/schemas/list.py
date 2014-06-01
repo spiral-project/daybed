ae69728faef74656716a73ec6ed016923c8a8b76
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-06-01 10:45:35 +0200

    List should just be a list of known itemtype (ref #54)

--- daybed/schemas/list.py
+++ daybed/schemas/list.py
@@ -1,6 +1,5 @@
 from pyramid.i18n import TranslationString as _
-from colander import (Sequence, SchemaNode, Length, String, drop, OneOf,
-                      Invalid)
+from colander import (SchemaNode, String, drop, OneOf)
 
 from .base import registry, TypeField, JSONList
 
@@ -14,18 +13,7 @@ class ListField(TypeField):
     def definition(cls, **kwargs):
         schema = super(ListField, cls).definition(**kwargs)
         schema.add(SchemaNode(String(),
-                              name='subtype',
+                              name='itemtype',
                               validator=OneOf(registry.names),
                               missing=drop))
-        schema.add(SchemaNode(Sequence(), TypeField.definition(),
-                              name='subfields',
-                              validator=Length(min=1),
-                              missing=drop))
-
-        def validator(node, value):
-            if 'subtype' not in value and 'subfields' not in value:
-                msg = u"No 'subtype' nor 'subfields'"
-                raise Invalid(node, msg)
-        schema.validator = validator
-
         return schema
--- daybed/tests/test_schemas_list.py
+++ daybed/tests/test_schemas_list.py
@@ -11,48 +11,23 @@ class ListFieldTest(unittest.TestCase):
         self.definition = {
             'name': u'tasks',
             'type': u'list',
-            'subfields': [{'type': u'boolean',
-                           'name': u'status',
-                           'hint': u'',
-                           'label': u'',
-                           'required': True}]}
+            'hint': u'An object',
+            'label': u'',
+            'required': True,
+            'itemtype': u'int'
+        }
         self.validator = schemas.ListField.validation(**self.definition)
 
-    def test_is_not_valid_if_no_subschema_nor_subtype(self):
-        self.definition.pop('subfields')
-        self.assertRaises(colander.Invalid,
-                          self.schema.deserialize,
-                          self.definition)
-
-    def test_is_defined_with_a_valid_subschema(self):
+    def test_can_be_defined_with_a_field_type(self):
         field = self.schema.deserialize(self.definition)
-        self.definition.update({
-            'label': u'',
-            'hint': u'A list of objects',
-            'required': True,
-        })
         self.assertDictEqual(self.definition, field)
 
-    def test_is_not_valid_if_subschema_invalid(self):
-        self.definition['subfields'][0].pop('type')
-        self.assertRaises(colander.Invalid,
-                          self.schema.deserialize,
-                          self.definition)
-
-    def test_is_not_valid_if_subschema_is_empty(self):
-        self.definition['subfields'] = []
-        self.assertRaises(colander.Invalid,
-                          self.schema.deserialize,
-                          self.definition)
-
-    def test_can_be_defined_with_a_field_type(self):
-        self.definition.pop('subfields')
-        self.definition['subtype'] = 'int'
-        self.schema.deserialize(self.definition)
-
     def test_is_not_valid_if_field_type_unknown(self):
-        self.definition.pop('subfields')
-        self.definition['subtype'] = 'asteroid'
+        self.definition['itemtype'] = 'asteroid'
         self.assertRaises(colander.Invalid,
                           self.schema.deserialize,
                           self.definition)
+
+    def test_validation_succeeds_if_fields_are_valid(self):
+        value = self.validator.deserialize('[1,3,4]')
+        self.assertEquals(value, [1, 3, 4])
