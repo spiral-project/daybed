44973f5cba7e240d35262c31ddb280399dfc9618
Alexis Métaireau <alexis@notmyidea.org>
2014-09-13 12:41:26 +0200

    Send JSON in the 40X errors

--- daybed/views/errors.py
+++ daybed/views/errors.py
@@ -1,14 +1,21 @@
+from pyramid.response import Response
 from pyramid.security import Everyone
-from pyramid.httpexceptions import HTTPUnauthorized, HTTPForbidden
 
 from cornice.cors import ensure_origin
 
 
 def forbidden_view(request):
     if not request.credentials_id or request.credentials_id == Everyone:
-        response = HTTPUnauthorized()
+        resp = Response(
+            '{"error": "401 Unauthorized",'
+            ' "msg": "You must be logged-in to access this page."}',
+            status='401 Unauthorized', content_type='application/json')
     else:
-        response = HTTPForbidden()
+        resp = Response(
+            '{"error": "403 Forbidden",'
+            ' "credentials_id": "%s", "msg": "Access to this resource is '
+            'Forbidden."}' % request.credentials_id, status='403 Forbidden',
+            content_type='application/json')
 
     # We need to re-apply the CORS checks done by Cornice, since we're
     # recreating the response from scratch.
@@ -17,5 +24,5 @@ def forbidden_view(request):
     service = services.get(pattern, None)
 
     request.info['cors_checked'] = False
-    resp = ensure_origin(service, request, response)
+    resp = ensure_origin(service, request, resp)
     return resp
