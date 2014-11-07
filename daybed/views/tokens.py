59e9e49cde6873825c77b657187285a91c415d13
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-11-07 16:05:02 +0100

    Add Basic Auth to POST /tokens to let the user get the same token for the same user:password.

--- daybed/views/tokens.py
+++ daybed/views/tokens.py
@@ -1,5 +1,8 @@
+import hashlib
+
 from cornice import Service
 
+from daybed.backends.exceptions import CredentialsAlreadyExist
 from daybed.tokens import get_hawk_credentials
 from daybed.views.errors import forbidden_view
 
@@ -10,10 +13,21 @@ token = Service(name='token', path='/token', description='Token')
 @tokens.post(permission='post_token')
 def post_tokens(request):
     """Creates a new token and store it"""
-    token, credentials = get_hawk_credentials()
-    request.db.store_credentials(token, credentials)
 
-    request.response.status = "201 Created"
+    # If we have an authorization header with the Basic or Token realm
+    # Use it as the base HKDF for building the same token
+    session_token = None
+    if request.authorization and request.authorization[0] in ["Basic", "Token"]:
+        session_token = hashlib.sha256("%s %s" % request.authorization[:2]).hexdigest()
+
+    token, credentials = get_hawk_credentials(session_token)
+
+    try:
+        request.db.store_credentials(token, credentials)
+    except CredentialsAlreadyExist:
+        request.response.status = "200 OK"
+    else:
+        request.response.status = "201 Created"
 
     return {
         'token': token,
