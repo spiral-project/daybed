18d1092809bb562c6c9b96a9d12f8f0600de25c9
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-11-11 17:50:58 +0100

    PEP8 and python 3.4

--- daybed/tests/test_schemas_validators.py
+++ daybed/tests/test_schemas_validators.py
@@ -14,6 +14,7 @@ class ValidatorTests(unittest.TestCase):
         self.request.db = mock.MagicMock()
         self.request.matchdict = mock.MagicMock()
         self.request.errors = Errors()
+        self.request.body = b''
 
     def test_adds_body_error_if_json_invalid(self):
         self.request.body = b'{wrong,"format"}'
@@ -38,7 +39,7 @@ class ValidatorTests(unittest.TestCase):
         self.assertIn('3.14', self.request.errors[0]['description'])
 
     def test_posted_data_is_postprocessed_recursively(self):
-        data =  {"records": [{"name": colander.null}]}
+        data = {"records": [{"name": colander.null}]}
         schema_mock = mock.Mock()
         schema_mock.deserialize.return_value = data
         validators.validator(self.request, schema_mock)
